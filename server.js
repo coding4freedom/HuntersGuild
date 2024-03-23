1109f18e040bc78b510e5ab97b97749d6776539d
coding4freedom <chawkins2010@hotmail.com>
2024-03-22 21:10:35 -0500

    Set up multi site template

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// http://localhost:4000
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -6,6 +7,14 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/instruct.html', (req, res) => {
+    res.sendFile(__dirname + '/public/instruct.html');
+});
+
+app.get('/contact.html', (req, res) => {
+    res.sendFile(__dirname + '/public/contact.html');
 });
 
 app.listen(PORT, () => {

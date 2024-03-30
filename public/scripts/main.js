163e2e05755bbaab39c43a1a153a594dc1263f57
coding4freedom <chawkins2010@hotmail.com>
2024-03-29 23:16:06 -0500

    connected external component gameContent.html and added game display layout

---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+fetch('components/gameContent.html')
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('simulation').innerHTML = html;
+    })
+    .catch(error => console.error('Error fetching content:', error));
